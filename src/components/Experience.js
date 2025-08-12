bf63adc1bd67c76b20d26a3181873f44dd39b532
Harshaun <harshaunkhehra@yahoo.com>
2025-08-12 15:36:59 -0700

    flixed background gradient to look smooth and no sudden color switch (fixed gradient)

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Experience = () => {
   return (
-    <div>
+    <div name="experience" className="bg-gradient-to-b from-black to-gray-800 w-full text-white pt-32 pb-32 md:min-h-[130vh]">
         Experience
     </div>
   )
